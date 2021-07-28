c5c1bda2fc91523b34ad92e965151800f44887a3
Muhammad Rizki Kurniawan <kurniawanrizzki@gmail.com>
2021-07-28 15:11:26 +0700

    feature (Scan): Implementation

--- app/src/main/java/com/advotics/addeen/scan/ScanFragment.kt
+++ app/src/main/java/com/advotics/addeen/scan/ScanFragment.kt
@@ -1,8 +1,42 @@
 package com.advotics.addeen.scan
 
+import android.os.Bundle
+import android.view.View
+import com.advotics.addeen.R
+import com.journeyapps.barcodescanner.DecoratedBarcodeView
 import com.noscale.cerberus.base.BaseFragment
 
 class ScanFragment: BaseFragment() {
+    override var mLayoutResource: Int = R.layout.fragment_scan
+
+    private var barcodeView: DecoratedBarcodeView? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        barcodeView = view.findViewById(R.id.dbv_scan_view)
+    }
+
+    override fun setUserVisibleHint(isVisibleToUser: Boolean) {
+        super.setUserVisibleHint(isVisibleToUser)
+        barcodeView?.let {
+            if (isVisibleToUser) {
+                it.resume()
+                return@let
+            }
+
+            it.pauseAndWait()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        barcodeView?.pauseAndWait()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        barcodeView?.resume()
+    }
 
     companion object {
         fun newInstance(): ScanFragment = ScanFragment()
